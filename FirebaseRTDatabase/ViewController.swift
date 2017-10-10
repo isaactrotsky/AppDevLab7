22d885279beb96c9d86d7dc8a8145272e8281a6f
rscovil <R@rs-macbook-pro-3.local>
2017-10-09 18:37:41 -0700

    Initial Commit

---
+++ FirebaseRTDatabase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseRTDatabase
+//
+//  Created by R on 10/9/17.
+//  Copyright © 2017 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
